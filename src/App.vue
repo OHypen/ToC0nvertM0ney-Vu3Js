05974c54dea6e381b5b030ca993bec1172f70453
Luis H <goldff36@gmail.com>
2021-04-07 13:06:57 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <h1>Conversor de Moedas</h1>
+    <div class="linha">
+      <Conversor moeda-a="USD" moeda-b="BRL"/>
+      <Conversor moeda-a="BRL" moeda-b="USD"/>
+    </div>
+    
+    <div class="linha">
+      <Conversor moeda-a="CAD" moeda-b="BRL"/>
+      <Conversor moeda-a="BRL" moeda-b="CAD"/>
+    </div>
+    
+    <div class="linha">
+      <Conversor moeda-a="EUR" moeda-b="BRL"/>
+      <Conversor moeda-a="BRL" moeda-b="EUR"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Conversor from "./components/Conversor.vue";
+export default {
+  name: "App",
+  components: {
+    Conversor
+  }
+};
+</script>
+
+<style>
+#app {
+  margin: auto;
+  max-width: 900px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  margin-top: 60px;
+
+}
+.linha{
+  margin-top: 40px;
+  display: flex;
+  justify-content: space-around;
+}
+h1 {
+  color: #ff79c6;
+}
+
+@media only screen and (max-width: 800px) {
+  .linha {
+    display: block;
+  }
+}
+</style>
